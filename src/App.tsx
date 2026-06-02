77b46e0620f10b104b0d0eafb9a2c7f19c1789c6
qahve9183 <qahve9813@gmail.com>
2026-06-02 23:28:09 +0300

    commit2

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ import { Create } from './pages/create';
 import { Profile } from './pages/profile';
 import { Forgot } from './pages/forgotpassword';
 import { Cod } from './pages/emailcod'
+import { Recovery } from './pages/passwordrecovery'
+import { Main } from './pages/main'
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/reg" replace />} />
+        <Route path='/' element={<Navigate to='/reg' replace />} />
         
-        <Route path="/reg" element={<Reg />} />
+        <Route path='/reg' element={<Reg />} />
         
-        <Route path="/login" element={<Log />} />
+        <Route path='/login' element={<Log />} />
 
-        <Route path="/create" element={<Create />} />
+        <Route path='/create' element={<Create />} />
 
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/profile' element={<Profile />} />
 
-        <Route path="/forgotpassword" element={<Forgot />}></Route>
+        <Route path='/forgotpassword' element={<Forgot />}></Route>
 
         <Route path='/emailcod' element={<Cod/>}></Route>
+
+        <Route path='/passwordrecovery' element={<Recovery/>}></Route>
+
+        <Route path='/main' element={<Main/>}></Route>
       </Routes>
     </Router>
   );
